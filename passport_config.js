5ee26257548857f442c137b5e86d7de2f6c093ab
Agostinhodossantos <agostinhodossantos27@gmail.com>
2020-12-28 12:41:36 +0200

    login

---
+++ passport_config.js
@@ -0,0 +1,32 @@
+const localStrategy = require('passport-local').Strategy
+const bcrypt = require('bcript');
+
+function initialize(passport , getUserByEmail) {
+
+    const authenticateUser = (email , password , done) => {
+        const user = getUserByEmail(email)
+        if(user == null) {
+            return done(null , false , {message: 'No user with that email'})
+        }
+
+        try {
+            if(await bcrypt.compare(password , user.password)) {
+                return done(null , user)
+            } else {
+                return done(null , false , {message: 'Password incorrect'})
+            }
+        } catch (error) {
+            return done(e)
+        }
+    }
+
+    passport.use(new localStrategy({usernameField: 'email'}), 
+    authenticateUser)
+
+    passport.serializeUser((user , done) => { })
+    passport.derializeUser((id , done) => { })
+
+}
+
+
+module.exports = initialize
